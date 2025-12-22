70dc126c8763fed2dbdb655410157a3196e18d67
Nassim <nassim6702@gmail.com>
2025-12-22 01:47:16 +0100

    First Commit - Set Archetecture - installl packages - add some dtos

---
+++ Restaurant-Management-System/RestaurantManagSyst.Service/Services/ClientService.cs
@@ -0,0 +1,14 @@
+﻿using RestaurantManagSyst.Service.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagSyst.Service.Services
+{
+    public class ClientService : IClientService
+    {
+    }
+}
